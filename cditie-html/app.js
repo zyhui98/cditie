923b34d9b4bdff0365eb76d27eb1e8342b40b596
zyhui98 <zyhui98@qq.com>
2018-05-08 11:57:47 +0800

    init

--- cditie-html/app.js
+++ cditie-html/app.js
@@ -1,3 +1,30 @@
 /**
  * Created by zhuyunhui on 5/8/2018.
  */
+CONFIG = {
+    weather:"http://localhost:8080/weather",
+    today:""
+}
+
+var app_weather = new Vue({
+    el: '#app_weather',
+    data: {
+        // 声明 message 为一个空值字符串
+        message: ''
+    }
+})
+
+function init(){
+    // 之后设置 `message`
+    Zepto.ajax({
+        type: "POST",
+        url: CONFIG.weather,
+        data: {},
+        success: function(data){
+            console.info(data);
+            app_weather.message = data;
+        }
+    });
+}
+
+init();
